4c57008beb9d749c6def8a919b2e3f8af22744f7
Drich011 <128674882+Drich011@users.noreply.github.com>
2023-06-16 13:49:23 +0800

    dsadad

--- js/main.js
+++ js/main.js
@@ -79,21 +79,121 @@ $(document).ready(function(){
         e.stopPropagation()
         return false
     })
-    // $('.buttons-chips .chips .btn-chip, .control-area-mobile .chips .btn-chip').on('touchstart',function(e){
-    //     e.stopImmediatePropagation()
-    //     layout.chipSelect(this)
-    // })
-    // $('.buttons-chips .chips, .control-area-mobile .chips, .top-control .chips').on('touchmove',function(e){
-    //     console.log("MOVING")
-    //     // e.stopPropagation() 
-    //   //  layout.handleTouchMove(this,e)
-    // })
-    // $('.buttons-chips .chips, .control-area-mobile .chips,  .top-control .chips').on('touchstart',function(e){
-    //     console.log("START")
-    //   //  layout.handleTouchStart(this,e)
-    // })
+    $('.buttons-chips .chips .btn-chip, .control-area-mobile .chips .btn-chip').on('touchstart',function(e){
+        e.stopImmediatePropagation()
+        layout.chipSelect(this)
+    })
+    $('.buttons-chips .chips, .control-area-mobile .chips, .top-control .chips').on('touchmove',function(e){
+        console.log("MOVING")
+        layout.isDragging = true
+        e.stopPropagation() 
+       layout.handleTouchMove(this,e)
+    })
+    $('.buttons-chips .chips, .control-area-mobile .chips,  .top-control .chips').on('touchstart',function(e){
+        console.log("START")
+      //  layout.handleTouchStart(this,e)
+    })
+    $('.buttons-chips .chips, .control-area-mobile .chips').on('touchend',function(e){
+        console.log("END")
+        console.log(e,"EEEEEEEE")
+       
+        
+        const targetElement = document.getElementById('TestChip');
+        console.log(targetElement, "ZDXZXCCZCX")
+        // Create a touch event+
+                // Get the current position of the mouse cursor+
+
+        var touch = e.changedTouches[0];  
+        console.log(touch,"touch")
+        var lastTouchX =  touch.clientX;
+        var lastTouchY = touch.clientY;
+        console.log(lastTouchX)
+        const touchEvent = new TouchEvent('touchstart', {
+        bubbles: true,
+        cancelable: true,
+        touches: [
+            new Touch({
+            identifier: Date.now(),
+            target: targetElement,
+            clientX: lastTouchX,
+            clientY: lastTouchY,
+            pageX: lastTouchX,
+            pageY: lastTouchY,
+            }),
+        ],
+        changedTouches: [
+            new Touch({
+            identifier: Date.now(),
+            target: targetElement,
+            clientX: lastTouchX,
+            clientY: lastTouchY,
+            pageX: lastTouchX,
+            pageY: lastTouchY,
+            }),
+        ],
+        });
+        // Find the element at the mouse position
+        var element = document.elementFromPoint(lastTouchX, lastTouchY);
+
+        // Dispatch the click event on the element
+        if (element) {
+            console.log(element)
+        element.dispatchEvent(touchEvent);
+}
+        // Dispatch the touch event to trigger it at the last touch location
+        targetElement.dispatchEvent(touchEvent);
+
+    })
+
+    // function mobileCLick(){
+    //         const targetElement = document.getElementById('TestChip');
+    //         console.log(targetElement, "ZDXZXCCZCX")
+    //         // Create a touch event+
+    //                 // Get the current position of the mouse cursor+
+
+    //         var touch = e.touches[0];   
+    //         console.log(touch,"touch")
+    //         var lastTouchX =  touch.clientX;
+    //         var lastTouchY = touch.clientY;
+    //         console.log(lastTouchX)
+    //         const touchEvent = new TouchEvent('touchstart', {
+    //         bubbles: true,
+    //         cancelable: true,
+    //         touches: [
+    //             new Touch({
+    //             identifier: Date.now(),
+    //             target: targetElement,
+    //             clientX: lastTouchX,
+    //             clientY: lastTouchY,
+    //             pageX: lastTouchX,
+    //             pageY: lastTouchY,
+    //             }),
+    //         ],
+    //         changedTouches: [
+    //             new Touch({
+    //             identifier: Date.now(),
+    //             target: targetElement,
+    //             clientX: lastTouchX,
+    //             clientY: lastTouchY,
+    //             pageX: lastTouchX,
+    //             pageY: lastTouchY,
+    //             }),
+    //         ],
+    //         });
+
+    //         // Dispatch the touch event to trigger it at the last touch location
+    //         targetElement.dispatchEvent(touchEvent);
+    //     }
+    
     // $('.buttons-chips .chips, .control-area-mobile .chips').on('touchend',function(){
     //     console.log("END")
+    //     $('.bet-area .area').mouseenter(function(){
+            
+    //             layout.isDragging = false
+    //             layout.detectParentDiv(this)
+    //             console.log("ZZZ")
+           
+    //     })
     //    // layout.handleTouchEnd()
     // })
     // $('.buttons-chips .chips, .top-control .chips').on('dragstart',function(e){
